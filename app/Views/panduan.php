6396738c0392926bedca8e7dc5595a3cc05bb042
Ganes yk <ganesyudhakusuma@gmail.com>
2024-05-19 07:07:51 +0700

    update LICENSE

--- app/Views/panduan.php
+++ app/Views/panduan.php
@@ -24,9 +24,6 @@
          </div>
          <ul class="navbar-nav ml-auto">
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-               <!-- <ul class="navbar-nav ms-auto mb-2 mb-lg-0 medium  fw-bolder">
-                  <a class="nav-link" href=""> Layanan Telepon </a>
-               </ul> -->
                <div class="col">
                   <a class="btn btn-info btn-md" href="<?= base_url('coba'); ?>">Panduan</a>
                </div>
